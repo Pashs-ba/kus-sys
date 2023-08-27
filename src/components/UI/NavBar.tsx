81070d5bf0318360d5afeea3c72a68a3a95ed1ad
Pashs-ba <pashs.ba@gmail.com>
2023-08-27 17:29:33 +0300

    add home page link

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -2,9 +2,22 @@ import {Link} from "react-router-dom";
 
 export default function NavBar({title, bootstrap_icon_name}: { title: string, bootstrap_icon_name?: string }) {
     return (
-        <nav className="navbar bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/"><i className={`bi ${bootstrap_icon_name} me-3`}/>{title}</Link>
+                {/*<Link className="nav-link active" to={"/"}>Home</Link>*/}
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to={"/"}>Домашняя страница</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
--- src/pages/admin/AdminRoot.tsx
+++ src/pages/admin/AdminRoot.tsx
@@ -6,7 +6,6 @@ export default function AdminRoot() {
         <div className={"container"}>
             <MessageBlock/>
             <div className="row gy-5 justify-content-center mt-5">
-
                 <HomePageLinkCard header={"Редактирование пользователей"} to={"/admin/user"}/>
                 <HomePageLinkCard header={"Редактирование классов"} to={"/admin/grade"}/>
                 <HomePageLinkCard header={"Редактирование журналов"} to={"/admin/journal"}/>
