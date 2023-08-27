cabd4288226f77be9d76d5ed14097993610b3380
Pashs-ba <pashs.ba@gmail.com>
2023-08-27 17:25:20 +0300

    change admin page

--- src/pages/admin/AdminRoot.tsx
+++ src/pages/admin/AdminRoot.tsx
@@ -5,11 +5,13 @@ export default function AdminRoot() {
     return (
         <div className={"container"}>
             <MessageBlock/>
-            <div className={"row justify-content-center align-items-center full-height"}>
+            <div className="row gy-5 justify-content-center mt-5">
+
                 <HomePageLinkCard header={"Редактирование пользователей"} to={"/admin/user"}/>
                 <HomePageLinkCard header={"Редактирование классов"} to={"/admin/grade"}/>
                 <HomePageLinkCard header={"Редактирование журналов"} to={"/admin/journal"}/>
                 <HomePageLinkCard header={"Редактирование планов"} to={"/admin/plan"}/>
+                <HomePageLinkCard header={"Редактирование предметов"} to={'/admin/subject'}/>
             </div>
 
         </div>
